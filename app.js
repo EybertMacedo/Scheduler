30bac3738d3a1b7ef7e7518cd78b108be98026f7
Alexis <emacedop@unsa.edu.pe>
2023-11-05 12:28:15 -0500

    updated storing logic

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const app = express();
 //static css
 app.use(express.static("public"));
 
-// Arreglo para almacenar los cursos agregados (simulando una base de datos)
-const cursosAgregados = [];
+// datos que seran utilizados en la aplicacion
+var cursosAgregados = [];
 const horasInicio = ["--","07:00","07:50","08:50","09:40","10:40","11:30","12:20","13:10","14:00","14:50","15:50","16:40","17:40","18:30","19:20","20:10"];
 const horasFin = ["--","07:50","08:40","09:40","10:30","11:30","12:20","13:10","14:00","14:50","15:40","16:40","17:30","18:30","19:20","20:10","21:00"];
-const dias = ["--","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+const dias = ["--","Lunes","Martes","Miercoles","Jueves","Viernes"];
+var info = {horasInicio:horasInicio, horasFin:horasFin, dias:dias, cursosAgregados :cursosAgregados};
 
-curso = [];
-cantidadGrupos = 0;
+var anadirCurso = false;
 
 // Configura EJS como motor de plantillas
 app.set('view engine', 'ejs');
@@ -23,46 +23,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Ruta para mostrar el formulario
 app.get('/', (req, res) => {
     res.render('index', { imprimirHorarios: imprimirHorarios, 
-        cursos: cursosAgregados,
-        curso: curso,
-        cantidadGrupos: cantidadGrupos,
-        dias: dias,
-        horasInicio: horasInicio,
-        horasFin: horasFin
+        info, 
+        anadirCurso
         });
 });
 
 // Ruta para mostrar los cursos agregados
 app.get('/cursos', (req, res) => {
-    res.json(curso);
+    res.json(cursosAgregados);
 });
 
 // Ruta para procesar la solicitud de agregar curso
 app.post('/agregar-curso', (req, res) => {
     // Aquí puedes manejar la lógica para agregar el curso a la base de datos
-    nombreCurso = req.body.nombreCurso;
-    cantidadGrupos = req.body.cantidadGrupos;
-    horariosDeCurso = [];
+    let nombreCurso = req.body.nombreCurso;
+    let cantidadGrupos = req.body.cantidadGrupos;
+    let horariosDeCurso = {};
+    let curso = ({ nombreCurso: nombreCurso, cantidadGrupos: cantidadGrupos, horariosDeCurso : horariosDeCurso});
+    cursosAgregados.push(curso);
+    anadirCurso = true;
 
-    curso.push({ nombreCurso: nombreCurso, cantidadGrupos: cantidadGrupos, horariosDeCurso : horariosDeCurso});
-            
     res.redirect('/');
 });
 
 app.post('/detallar-curso', (req, res) => {
     // Aquí puedes manejar la lógica para agregar el curso a la base de datos
     temp = {};
+    let cursoActual = cursosAgregados[cursosAgregados.length - 1];
+
     for (let data in req.body) {
         temp[data] = req.body[data];
     };    
-    horariosDeCurso.push(temp);    
-    cantidadGrupos = 0;
+    cursoActual.horariosDeCurso = temp;
+    anadirCurso = false;
 
     res.redirect('/');
 });
-
-
-
 
 //funciones
 
---
+++ test.js
@@ -0,0 +1,5 @@
+xd = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+dict = {eybert: xd}
+xd[0] = 100
+
+console.log(dict.eybert[0])
